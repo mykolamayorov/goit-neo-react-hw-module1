8056c1784b7850c99df9ca0c18743ea708fd494c
mykolamayorov <mykolamayorov@gmail.com>
2025-11-30 15:18:48 +0100

    submit profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={styles.userProfile}>
+      <div className={styles.userInfo}>
+        <img className={styles.userPicture} src={image} alt="User avatar" />
+        <p className={styles.userName}>{name}</p>
+        <p className={styles.userTag}>@{tag}</p>
+        <p className={styles.userLocation}>{location}</p>
+      </div>
+
+      <ul className={styles.userStats}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
